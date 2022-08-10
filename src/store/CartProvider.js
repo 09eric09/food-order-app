98a50b32edfd934c833492889014ee636061b14e
09eric09 <09eric09@gmail.com>
2022-08-09 20:46:39 -0700

    [144] added cart reducer for adding items

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import CartContext from './cart-context';
 
+//default state values that will be updated with the reducer function
+let initialCart = {
+  items: [],
+  totalAmount: 0,
+}
+
+const cartReducer = (state, action) => {
+  if (action.val === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedAmount = state.totalAmount + action.item.price * action.item.amount;
+
+    return{
+      items: updatedItems,
+      totalAmount: updatedAmount,
+    };
+  }
+  return initialCart;
+}
+
 const CartProvider = (props) => {
-    const addItemToCartHandler = (item) => {}
-    const removeItemFromCartHandler = (item) => {}
-    
+
+  const [cartState, dispatchCart] = useReducer(cartReducer, initialCart);
+
+  const addItemToCartHandler = (item) => {
+    dispatchCart({val:'ADD', item:item});
+  }
+
+  const removeItemFromCartHandler = (item) => {
+
+  }
+
+  //Default state values we pass into the CartContext.Provider
     const cartProviderValue = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler, 
         removeItem: removeItemFromCartHandler,
     };
+
   return (
     //Creating this here instead of managing context in App.js
     <CartContext.Provider value={cartProviderValue}>
