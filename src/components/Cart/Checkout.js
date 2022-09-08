8a9e7c4ac861b91b0b66e2dd80b81033b96215d8
09eric09 <09eric09@gmail.com>
2022-09-07 21:46:19 -0700

    grabbed input value and stored in state

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './Checkout.module.css';
 
 const Checkout = (props) => {
+    const [nameInput, setNameInput] = useState('');
+    const [streetInput, setStreetInput] = useState('');
+    const [postalInput, setPostalInput] = useState('');
+    const [cityInput, setCityInput] = useState('');
+
     const confirmHandler = (e) => {
         e.preventDefault();
-        
+
+        let orderInfo = {
+            name: nameInput,
+            street: streetInput,
+            postal: postalInput,
+            city: cityInput,
+        }
+
+        console.log(orderInfo);
     }
 
   return (
@@ -12,22 +25,24 @@ const Checkout = (props) => {
     <form onSubmit={confirmHandler}>
         <div className={classes.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id='name'/>
+            <input onChange={(e)=> setNameInput(e.target.value)} type="text" id='name' value={nameInput}/>
         </div>
         <div className={classes.control}>
             <label htmlFor="name">Street</label>
-            <input type="text" id='name'/>
+            <input onChange={(e)=> setStreetInput(e.target.value)} type="text" id='name' value={streetInput}/>
         </div>
         <div className={classes.control}>
             <label htmlFor="name">Postal Code</label>
-            <input type="number" id='name'/>
+            <input onChange={(e)=> setPostalInput(e.target.value)} type="number" id='name' value={postalInput}/>
         </div>
         <div className={classes.control}>
             <label htmlFor="name">City</label>
-            <input type="text" id='name'/>
+            <input onChange={(e)=> setCityInput(e.target.value)} type="text" id='name' value={cityInput}/>
         </div>
+        <div className={classes.actions}>
         <button>Confirm</button>
         <button onClick={props.onClose} type='button'>Cancel</button>
+        </div>
     </form>
     </>
   )
